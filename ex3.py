d70c585133ebbfc9dabec2ab722c0b4ecb1b3a24
Meurig Freeman <me@meurig.com>
2010-12-03 17:30:12 +0000

    Exercise 3 - Extra Credit (floating point)

--- ex3.py
+++ ex3.py
@@ -1,27 +1,27 @@
 print "I will now count my chickens:"
 
 # 30 / 6 = 5, 25 + 5 = 30.
-print "Hens", 25 + 30 / 6
+print "Hens", 25.0 + 30.0 / 6.0
 # 25 * 5 = 75, 75 % 4 = 3 (72 = 18 *4), 100 - 3 = 97
-print "Roosters", 100 - 25 * 3 % 4
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 print "Now I will count the eggs:"
 
 # 4 % 2 = 0, 1 / 4 = 0, 3 + 2 + 1 - 5 + 0 - 0 + 6 = 7
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
-print "Is it true that 3 + 2 < 5 - 7?"
-print 3 + 2 < 5 - 7
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+print 3.0 + 2.0 < 5.0 - 7.0
 
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+print "What is 3.0 + 2.0?", 3.0 + 2.0
+print "What is 5.0 - 7.0?", 5.0 - 7.0
 
 print "Oh, that's why it's False."
 
 print "How about some more."
 
-print "Is it greater?", 5 > -2
+print "Is it greater?", 5.0 > -2.0
 
-print "Is it greater or equal?", 5 >= -2
+print "Is it greater or equal?", 5.0 >= -2.0
 
-print "Is it less or equal?", 5 <= -2
+print "Is it less or equal?", 5.0 <= -2.0
